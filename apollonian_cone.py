e0a60e5c64cdc5e0007a601d35fc9175d122ae3c
macbuse <greg.mcshane@gmail.com>
2016-10-31 11:38:41 +0100

    Update apollonian_cone.py

--- apollonian_cone.py
+++ apollonian_cone.py
@@ -6,6 +6,7 @@ rr = 50
 
 def apply_boolean(obj_A, obj_B, bool_type = 'INTERSECT'):
     
+    print('+++',obj_A, obj_B)
     bpy.ops.object.select_all(action='DESELECT')
     obj_A.select= True
     
@@ -61,15 +62,15 @@ def mk_cubes(scale_factor=40):
    
 
     cubes = [bb]
-    for rot in [-np.pi/6,-np.pi/3, -np.pi/2] :
+    for rot in [-np.pi/6,-np.pi/3 -.2, -np.pi/2] :
         bpy.ops.object.duplicate()
         bb = bpy.context.scene.objects.active
         bb.rotation_euler = [0,rot,0]
         cubes.append(bb)
     
     cubes[0].location = [0,0,12]
-    cubes[1].location = [0,0,6]
-    cubes[2].location = [0,0,-4]
+    cubes[1].location = [0,0,8]
+    cubes[2].location = [0,0,-5]
     cubes[3].location = [10,0,0]
     
     return cubes
@@ -85,13 +86,12 @@ cone.scale = [rr,rr,rr]
 bpy.ops.mesh.primitive_cylinder_add(location = [0,0,0] ) 
 peg = bpy.context.scene.objects.active
 peg_width = 1.5
-peg.scale = [peg_width,peg_width,15]
-peg.location = [0,0,-1]
+
 
 
 boxes = mk_cubes()
 
-cones = [cone]
+cones = []
 for bb in boxes:
     cones.append(cut(cone,bb))
 
@@ -102,24 +102,27 @@ for box in  boxes:
 #get rid of the
 bpy.ops.object.delete() 
 
+peg.scale = [peg_width,peg_width,6]
+peg.location = [0,0,8]
 #cut holes using the peg
-for obj in cones[:4]:
+for obj in cones[:2]:
     apply_boolean(obj,peg, bool_type = 'DIFFERENCE')
     
 
 #shrink the peg before gluing it to the base
 peg.scale = [peg_width - .2,peg_width - .2 ,6]
-apply_boolean(cones[4],peg, bool_type = 'UNION')
+apply_boolean(cones[2],peg, bool_type = 'UNION')
     
 
 peg.rotation_euler = [0,np.pi/2,0]
-peg.location = [8,0,-16]
+peg.location = [3,0,-16]
 
-peg.scale = [peg_width,peg_width,6]
+peg.scale = [peg_width,peg_width,11]
 apply_boolean(cone,peg, bool_type = 'DIFFERENCE')
+apply_boolean(cones[2],peg, bool_type = 'DIFFERENCE')
 
-peg.scale = [peg_width - .2,peg_width - .2 ,6]
-apply_boolean(cones[4],peg, bool_type = 'UNION')
+peg.scale = [peg_width - .2,peg_width - .2 ,11]
+apply_boolean(cones[3],peg, bool_type = 'UNION')
 
 
 bpy.ops.object.select_all(action='DESELECT')
